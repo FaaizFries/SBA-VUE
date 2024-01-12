cd8e94091a63993d2083708ed4388ec70e034ed8
Faaiz Ali <146028028+FaaizFries@users.noreply.github.com>
2024-01-12 12:34:13 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+
+nav {
+  padding: 30px;
+  background: url('https://png.pngtree.com/background/20210712/original/pngtree-mysterious-cosmic-space-galaxy-nebula-background-picture-image_1179108.jpg') center/cover no-repeat;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  background-color: white;
+}
+
+nav a.router-link-exact-active {
+  color: blue;
+
+}
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h2>Login</h2>
+     <p v-if="showMessage">{{ message }}</p>
+    <button @click="toggleMessage">Toggle Message</button>
+    <p>{{ message }}</p>
+    <input v-model="username" placeholder="Enter username" />
+    <p>Username uses Data Binding: {{ customProp }}</p>
+    <form @submit.prevent="login">
+      <label for="username">Username:</label>
+      <input type="text" v-model="username" required />
+
+      <label for="password">Password:</label>
+      <input type="password" v-model="password" required />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Welcome to the login page!",
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log("Logging in...");
+    },
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  padding: 30px;
+  background: url('https://png.pngtree.com/background/20210712/original/pngtree-mysterious-cosmic-space-galaxy-nebula-background-picture-image_1179108.jpg') center/cover no-repeat;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  background-color: white;
+}
+
+nav a.router-link-exact-active {
+  color: blue;
+
+}
+</style>
